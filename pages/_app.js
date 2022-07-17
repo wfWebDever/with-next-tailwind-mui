1646ac6c9ec425f1b2b998863f2eac638f26d301
weifeng <xinyilan2005@gmail.com>
2022-07-17 11:23:34 +0800

    feature: update tailwindcss font family useful

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { StyledEngineProvider } from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
+import Layout from '../components/layout'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StyledEngineProvider injectFirst>
+      {/* Your component tree. Now you can override MUI's styles. */}
+      <CssBaseline />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </StyledEngineProvider>
+  )
 }
 
 export default MyApp
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}'
+    ]
+  },
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  important: '#app',
+  theme: {
+    fontFamily: {
+      edusa: ['EduSABeginner', ...defaultTheme.fontFamily.sans]
+    },
+    colors: {
+      gray: '#f3f4f6',
+      red: '#ef4444',
+      primary: '#fff',
+      secondary: '#fff',
+      neutral: '#fff'
+    },
+    extend: {}
+  },
+  plugins: []
+}
